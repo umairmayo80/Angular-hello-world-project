c7e2b1665351d8ef385214a733e1df8f20cdaab4
umairmayo80 <aliumer1080@gmail.com>
2023-08-21 12:43:09 +0500

    email validator implementation complete, custom validation using custom directive example

--- src/app/emailValidator/emailvalidator.directive.ts
+++ src/app/emailValidator/emailvalidator.directive.ts
@@ -1,10 +1,31 @@
 import { Directive } from '@angular/core';
+import {
+  AbstractControl,
+  NG_VALIDATORS,
+  ValidationErrors,
+  Validator,
+} from '@angular/forms';
 
 @Directive({
-  selector: '[appEmailvalidator]'
+  selector: '[appEmailvalidator]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: EmailvalidatorDirective,
+      multi: true,
+    },
+  ],
 })
-export class EmailvalidatorDirective {
+export class EmailvalidatorDirective implements Validator {
+  constructor() {}
 
-  constructor() { }
-
+  validate(control: AbstractControl): ValidationErrors | null {
+    const value = control.value as string;
+    if (value.includes('test')) {
+      return {
+        invalidEmail: true,
+      };
+    }
+    return null;
+  }
 }
