5d4f1badbd0ff0136b32203e4075e6a62313c0ff
umairmayo80 <aliumer1080@gmail.com>
2023-08-23 14:30:00 +0500

    Added nested routing in rooms using child route

--- src/app/rooms/rooms-routing.module.ts
+++ src/app/rooms/rooms-routing.module.ts
@@ -5,16 +5,24 @@ import { RoomsBookingComponent } from './rooms-booking/rooms-booking.component';
 import { RoomsComponent } from './rooms.component';
 
 const routes: Routes = [
-    // the order of routes matter alot
-    {path: 'rooms', component: RoomsComponent}, 
-    {path:'rooms/add',component:RoomsAddComponent},
-    {path:'rooms/:id',component: RoomsBookingComponent}, //here :id is dynamic
-    {path: '', redirectTo:'/rooms',pathMatch:'full'}, //default root
-  
+  { path: 'rooms/add', component: RoomsAddComponent },
+  // the order of routes matter alot
+  {
+    path: 'rooms',
+    component: RoomsComponent,
+    children: [
+      // { path: 'add', component: RoomsAddComponent }, it will also work and it must be before the dynamic route
+      { path: ':id', component: RoomsBookingComponent },
+      
+    ],
+  },
+
+  //here :id is dynamic
+  { path: '', redirectTo: '/rooms', pathMatch: 'full' }, //default root
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class RoomsRoutingModule { }
+export class RoomsRoutingModule {}
