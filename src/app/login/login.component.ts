48d95a32a13b02cac005207be2fe673ab4fde518
umairmayo80 <aliumer1080@gmail.com>
2023-08-23 12:15:58 +0500

    Added Router service in login to redirect to another page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,12 +11,17 @@ export class LoginComponent implements OnInit {
   email: string='';
   password: string='';
 
-  constructor() { }
+  // Router from programtic routing service, Router is service imported from '@angular/router';
+  constructor(private router: Router) { }
 
 
   login():void{
     if(this.password === 'admin' && this.email === 'admin@gmail.com'){
       alert('Login Success')
+      // this.router.navigate(["/rooms","add"])
+      // we can do the same using navigateByUrl
+      this.router.navigateByUrl("/rooms/add");
+      
     }
     else{
       alert('Wrong Credentials')
