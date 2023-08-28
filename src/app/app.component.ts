5d1e33fc87d5952fc7f940769e27157f9411cac0
umairmayo80 <aliumer1080@gmail.com>
2023-08-28 11:06:08 +0500

    Provided any example and injection token example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,62 @@
-import { Component , OnInit,ViewChild, AfterViewInit, ViewContainerRef, ElementRef, Inject} from '@angular/core';
-import {localStorageToken} from './localStorage.token'
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  AfterViewInit,
+  ViewContainerRef,
+  ElementRef,
+  Inject,
+} from '@angular/core';
+import { localStorageToken } from './localStorage.token';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   // template: `<h1 style='text-align: center'>Hello world </h1>
-  
+
   // <p style='text-align:center'> Angular 13 based Sample Project </p>
   // `,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, AfterViewInit{
-
-  constructor(@Inject(localStorageToken) private localStorage: Storage){
-
-  }
-
+export class AppComponent implements OnInit, AfterViewInit {
+  constructor(
+    @Inject(localStorageToken) private localStorage: Storage,
+    private router: Router
+  ) {}
 
   title = 'hotelinvertory';
   role = 'admin';
 
-  @ViewChild('user',{read: ViewContainerRef}) vcr!: ViewContainerRef;
-  @ViewChild('templateReferenceName',{ static:true}) htmlElement!: ElementRef;
-  
-
+  @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef;
+  @ViewChild('templateReferenceName', { static: true })
+  htmlElement!: ElementRef;
 
   // only works if the viewChild static property is true
   ngOnInit(): void {
-    // now we can create component on runtime /dynamically
-      // const componentRef = this.vcr.createComponent(RoomsComponent)
-      console.log('onInit')
-      console.log(this.htmlElement)
-      this.htmlElement.nativeElement.innerText = 'This text is created using ngTemplateReference'
 
-      this.localStorage.setItem('name','KingsMen-Hotel');
+    
+    this.router.events.subscribe(event =>{
+      console.log(event)
+    });
+
+    // now we can create component on runtime /dynamically
+    // const componentRef = this.vcr.createComponent(RoomsComponent)
+    console.log('AppComponent-onInit');
+    console.log(this.htmlElement);
+    this.htmlElement.nativeElement.innerText =
+      'This text is created using ngTemplateReference';
+
+    this.localStorage.setItem('name', 'KingsMen-Hotel');
+
+
   }
 
   ngAfterViewInit(): void {
-        // now we can create component on runtime /dynamically
-        // const componentRef = this.vcr.createComponent(RoomsComponent)      
-        console.log('AfterViewInit')
-        // now we can access the properties of the component
-        // componentRef.instance.numberOfRooms = 50;
+    // now we can create component on runtime /dynamically
+    // const componentRef = this.vcr.createComponent(RoomsComponent)
+    console.log('AfterViewInit');
+    // now we can access the properties of the component
+    // componentRef.instance.numberOfRooms = 50;
   }
-
-
 }
